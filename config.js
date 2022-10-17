d3f5cd87c9a25c1fb58532b8eaa0f24e20b40b8b
trong_hoang <hwang1608.203@gmail.com>
2022-10-17 22:58:23 +0700

    hwang

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '<3 Hê sờ lô hơ sờ LiiLy ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Được thôi',
+    title: 'Em có biết em đáng iu đến nhường nào không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em ghét Anh đó nha :> ',
+    btnYes: 'Cóa , em muốn biết💖💖💖',
+    btnNo: 'Thôi dẹp đi cái trò chechou ',
+    question:'Nhiều lúc call với em được nghe tiếng em thở :))) , nghe em ngáy khịt khịt đáng iu thật sự hihi',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Đừng hòng mắng tuiiiiii hehhehe',
+    mess: 'Anh biết mà, chắc chắn là em có tức khi thấy nma em vẫn là bé Lii đáng yêu thật sự , giọng ngọt ngào ,...🥰',
+    messDesc: '💬💬 Xong hết rồi thì đừng nhắn mess mắng tui nha💬💬',
+    btnAccept: 'Được thoai💖',
+}
